29904a06d0e15d92ced5d48ab8565fbe175c1af6
durpintm <durpinthapa@gmail.com>
2024-06-08 17:07:00 -0400

    Add prisma

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ const client = new Client({
 //   await client.connect();
 
 //   const result = await client.query(`
-//   CREATE TABLE users (
-//     id SERIAL PRIMARY KEY,
-//     username VARCHAR(50) UNIQUE NOT NULL,
-//     email VARCHAR(255) UNIQUE NOT NULL,
-//     password VARCHAR(255) NOT NULL,
-//     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-//       );
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY,
+//   username VARCHAR(50) UNIQUE NOT NULL,
+//   email VARCHAR(255) UNIQUE NOT NULL,
+//   password VARCHAR(255) NOT NULL,
+//   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//     );
 //   `);
 
 //   console.log(result);
