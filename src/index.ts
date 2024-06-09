6be87c3c417b449fac69f9081ff74cb065f8c585
durpintm <durpinthapa@gmail.com>
2024-06-08 21:45:52 -0400

    Insert and Update User

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+async function insertUser(
+  email: string,
+  password: string,
+  firstname: string,
+  lastname: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      email,
+      password,
+      firstname,
+      lastname,
+    },
+    select: {
+      id: true,
+      firstname: true,
+      password: true,
+    },
+  });
+
+  console.log(res);
+}
+// insertUser("sagarghimire@gmail.com", "password123", "Sagar", "Ghimire");
+
+interface UpdateParams {
+  firstname: string;
+  lastname: string;
+}
+async function updateUser(
+  username: string,
+  { firstname, lastname }: UpdateParams
+) {
+  const res = await prisma.user.update({
+    where: { email: username },
+    data: {
+      lastname,
+    },
+  });
+  console.log(res);
+}
+
+// updateUser("durpinthapa@gmail.com", { firstname: "Durp", lastname: "Thapa M" });
