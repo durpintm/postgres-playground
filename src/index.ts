5b8153f03b905e029e2a114f83b68e593af60167
durpintm <durpinthapa@gmail.com>
2024-06-08 21:12:27 -0400

    Added migration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import { Client } from "pg";
 
-const client = new Client({
-  connectionString:
-    "postgresql://testdb_owner:ISVRHQX01kBf@ep-black-sun-a5jk9tmv.us-east-2.aws.neon.tech/testdb?sslmode=require",
-});
